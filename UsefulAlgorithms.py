56dc6cae1153c153d9045136a2dba1a8efbfcd5e
adirao-projects <adi.rao@mail.utoronto.ca>
2022-12-02 16:54:14 -0500

    QOL changes

--- UsefulAlgorithms.py
+++ UsefulAlgorithms.py
@@ -5,16 +5,26 @@ from typing import Any, Dict, Tuple, List, Set, Optional, TextIO
 # Eventually will look at these https://www.geeksforgeeks.org/top-algorithms-and-data-structures-for-competitive-programming/
 
 
-def binary_search(lst: List[Any]) -> List[Any]:
+def binary_search(lst: List[Any]) -> Any:
 	"""
-		PreConditions:
-			list must be presorted!!!
+		Parameters
+	    ----------
+	    lst : List[Any]
+	        List of elements that need to be search,
+	        the list MUST BE SORTED!!!
+
+	    Returns
+	    -------
+	    Todo: Implement
 	
-		Description:
+		Description
+		-----------
 			Binary Search is a search algorithm that finds the 
 			position of a target value within a sorted array.
 
-		Time Complexity : O(log n)(base 2)
+		Time Complexity
+		---------------
+			O(log n)(base 2)
 	"""
 
 	raise NotImplementedError
@@ -22,16 +32,30 @@ def binary_search(lst: List[Any]) -> List[Any]:
 
 def ternary_search(lst: List[Any]) -> List[Any]:
 	"""
-		PreConditions:
+		Parameters
+	    ----------
+	    lst : List[Any]
+	        List of elements that need to be search,
+	        the list MUST BE SORTED!!!
+
+	    Returns
+	    -------
+	    Todo: Implement
+
+		PreConditions
+		-------------
 			lst must be presorted!!!
 
-		Description:
+		Description
+		-----------
 			This algorithm is used to find an element in an 
 			array. It is similar to binary search where we 
 			divide the array into two parts but in this algorithm. 
 			In this, we divide the given array into three parts.
 
-		Time Complexity : O(log n)(base 3)
+		Time Complexity
+		--------------- 
+			O(log n)(base 3)
 	"""
 
 	raise NotImplementedError
@@ -39,14 +63,28 @@ def ternary_search(lst: List[Any]) -> List[Any]:
 
 def quick_sort(lst: List[Any]) -> List[Any]:
 	"""
-		Description:
+		Parameters
+	    ----------
+	    lst : List[Any]
+	        List of elements that need to be search,
+	        the list MUST BE SORTED!!!
+
+	    Returns
+	    -------
+	    sorted_lst: List[Any]
+	    	Sorted list of elements
+
+		Description
+		-----------
 			Quick Sort Algorithm is based on divide and 
 			conquer approach of problem solving. This 
 			algorithm is very useful to sort a sequence 
 			of integers of characters.
 
-		Time Complexity : O(nlog n) // Average case
-		Time Complexity : O(n^2) // Worst case
+		Time Complexity
+		---------------
+			O(nlog n) // Average case
+			O(n^2) // Worst case
 	"""
 
 	raise NotImplementedError
@@ -54,13 +92,16 @@ def quick_sort(lst: List[Any]) -> List[Any]:
 
 def merge_sort(lst: List[Any]) -> List[Any]:
 	"""
-		Description:
+		Description
+		-----------
 			Merge Sort Algorithm is based on divide and 
 			conquer approach of problem solving. This 
 			algorithm is very useful to sort a sequence 
 			of integers of characters.
 
-		Time Complexity : O(nlog n)
+		Time Complexity
+		---------------
+			O(nlog n)
 	"""
 
 	raise NotImplementedError
@@ -68,13 +109,16 @@ def merge_sort(lst: List[Any]) -> List[Any]:
 
 def rabin_karp(pattern: str, text: str) -> str:
 	"""
-		Description:
+		Description
+		-----------
 			It is a string-searching algorithm created by 
 			Richard M. Karp and Michael O. Rabin that uses 
 			hashing to find an exact match of a pattern 
 			string in a text.
 
-		Time Complexity : O(n)
+		Time Complexity
+		---------------
+			O(n)
 	"""
 
 	raise NotImplementedError
@@ -82,12 +126,15 @@ def rabin_karp(pattern: str, text: str) -> str:
 
 def kmp(word: str, text: str) -> str:
 	"""
-		Description:
+		Description
+		-----------
 			It is a searching algorithm that searches for 
 			occurrences of a “word” W within a main “text 
 			string” S.
 
-		Time Complexity : O(n)
+		Time Complexity
+		---------------
+			 O(n)
 	"""
 
 	raise NotImplementedError
